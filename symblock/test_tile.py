317904026fbf94be5f0c3fce5bf78e366b413060
Andreas Copan <avcopan@gmail.com>
2015-10-17 14:17:30 -0400

    tiles complete

--- symblock/test_tile.py
+++ symblock/test_tile.py
@@ -1,13 +1,36 @@
 from tile import Tile
-import numpy as np
+import numpy  as np
+import pytest as pt
 
-def test___getitem__1():
+def test___init_____01():
+  T = Tile((5, 7))
+  assert T._shape == (5, 7)
+
+def test___init_____02():
+  T = Tile((5, 7), np.zeros((5, 7)))
+  assert T._shape == T._array.shape
+
+def test___init_____03():
+  with pt.raises(TypeError ) as message:
+    Tile((5, 7), "this is a string, not a numpy array")
+  assert str(message).endswith("Cannot initialize Tile with object of type 'str'")
+
+def test___init_____04():
+  with pt.raises(ValueError) as message:
+    Tile((5, 7), np.zeros((7, 5)))
+  assert str(message).endswith("Cannot initialize (5, 7)-shaped Tile with an array of shape (7, 5)")
+
+def test_get_shape__01():
+  T = Tile((5, 7))
+  assert T.get_shape() == (5, 7)
+
+def test___getitem__01():
   T = Tile((5, 7))
   for i in range(5):
     for j in range(7):
       assert T[i,j] == 0.0
 
-def test___setitem__1():
+def test___setitem__01():
   T = Tile((5, 7))
   for i in range(5):
     for j in range(7):
@@ -16,21 +39,21 @@ def test___setitem__1():
     for j in range(7):
       assert T[i,j] == 7*i + j
 
-def test___nonzero__1():
+def test_is_empty___01():
   T = Tile((5, 7))
-  assert bool(T) == False
+  assert T.is_empty() == True
 
-def test___repr_____1():
+def test___repr_____01():
   T = Tile((5, 7))
   assert str(T) == 'Empty Tile'
 
-def test___getitem__2():
+def test___getitem__02():
   T = Tile((5, 7), np.arange(5*7).reshape((5,7)))
   for i in range(5):
     for j in range(7):
       assert T[i,j] == 7*i + j
 
-def test___getitem__2():
+def test___getitem__02():
   T = Tile((5, 7), np.arange(5*7).reshape((5,7)))
   for i in range(5):
     for j in range(7):
@@ -39,16 +62,401 @@ def test___getitem__2():
     for j in range(7):
       assert T[i,j] == i - j
 
-def test___nonzero__2():
+def test_is_empty___02():
   T = Tile((5, 7), np.arange(5*7).reshape((5,7)))
-  assert bool(T) == True
+  assert T.is_empty() == False
 
-def test___repr_____2():
+def test___repr_____02():
   T = Tile((5, 7), np.arange(5*7).reshape((5,7)))
   assert str(T) == """\
-array([[ 0,  1,  2,  3,  4,  5,  6],
-       [ 7,  8,  9, 10, 11, 12, 13],
-       [14, 15, 16, 17, 18, 19, 20],
-       [21, 22, 23, 24, 25, 26, 27],
-       [28, 29, 30, 31, 32, 33, 34]])\
+[[ 0  1  2  3  4  5  6]
+ [ 7  8  9 10 11 12 13]
+ [14 15 16 17 18 19 20]
+ [21 22 23 24 25 26 27]
+ [28 29 30 31 32 33 34]]\
 """
+
+def test___radd_____01():
+  L = 2.0
+  R = Tile((5, 7))
+  T = L + R
+  assert isinstance(T, Tile)
+  assert T.is_empty()
+
+def test___radd_____02():
+  L = 2.0
+  R = Tile((5, 7), np.ones((5, 7)))
+  T = L + R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 3.0
+
+def test___add______01():
+  L = Tile((5, 7))
+  R = 2.0
+  T = L + R
+  assert isinstance(T, Tile)
+  assert T.is_empty()
+
+def test___add______02():
+  L = Tile((5, 7), np.ones((5, 7)))
+  R = 2.0
+  T = L + R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 3.0
+
+def test___add______03():
+  L = Tile((5, 7))
+  R = Tile((5, 7))
+  T = L + R
+  assert isinstance(T, Tile)
+  assert T.is_empty()
+
+def test___add______04():
+  L = Tile((5, 7))
+  R = Tile((5, 7), np.ones((5, 7)))
+  T = L + R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 1.0
+
+def test___add______05():
+  L = Tile((5, 7), np.ones((5, 7)))
+  R = Tile((5, 7))
+  T = L + R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 1.0
+
+def test___add______06():
+  L = Tile((5, 7))
+  R = Tile((7, 5))
+  with pt.raises(ValueError) as message:
+    T = L + R
+  assert str(message).endswith("Cannot __add__ Tiles with mismatched shapes (5, 7) and (7, 5)")
+
+def test___add______07():
+  L = Tile((5, 7))
+  R = Tile((7, 5), np.ones((7, 5)))
+  with pt.raises(ValueError) as message:
+    T = L + R
+  assert str(message).endswith("Cannot __add__ Tiles with mismatched shapes (5, 7) and (7, 5)")
+
+def test___add______08():
+  L = Tile((7, 5), np.ones((7, 5)))
+  R = Tile((5, 7))
+  with pt.raises(ValueError) as message:
+    T = L + R
+  assert str(message).endswith("Cannot __add__ Tiles with mismatched shapes (7, 5) and (5, 7)")
+
+def test___add______09():
+  L = Tile((5, 7),   np.ones((5, 7)))
+  R = Tile((5, 7), 2*np.ones((5, 7)))
+  T = L + R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 3.0
+
+def test___add______10():
+  L = Tile((5, 7), 2*np.ones((5, 7)))
+  R = Tile((5, 7),   np.ones((5, 7)))
+  T = L + R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 3.0
+
+def test___rsub_____01():
+  L = 2.0
+  R = Tile((5, 7))
+  T = L - R
+  assert isinstance(T, Tile)
+  assert T.is_empty()
+
+def test___rsub_____02():
+  L = 2.0
+  R = Tile((5, 7), np.ones((5, 7)))
+  T = L - R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 1.0
+
+def test___sub______01():
+  L = Tile((5, 7))
+  R = 2.0
+  T = L - R
+  assert isinstance(T, Tile)
+  assert T.is_empty()
+
+def test___sub______02():
+  L = Tile((5, 7), np.ones((5, 7)))
+  R = 2.0
+  T = L - R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == -1.0
+
+def test___sub______03():
+  L = Tile((5, 7))
+  R = Tile((5, 7))
+  T = L - R
+  assert isinstance(T, Tile)
+  assert T.is_empty()
+
+def test___sub______04():
+  L = Tile((5, 7))
+  R = Tile((5, 7), np.ones((5, 7)))
+  T = L - R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == -1.0
+
+def test___sub______05():
+  L = Tile((5, 7), np.ones((5, 7)))
+  R = Tile((5, 7))
+  T = L - R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 1.0
+
+def test___sub______06():
+  L = Tile((5, 7))
+  R = Tile((7, 5))
+  with pt.raises(ValueError) as message:
+    T = L - R
+  assert str(message).endswith("Cannot __sub__ Tiles with mismatched shapes (5, 7) and (7, 5)")
+
+def test___sub______07():
+  L = Tile((5, 7))
+  R = Tile((7, 5), np.ones((7, 5)))
+  with pt.raises(ValueError) as message:
+    T = L - R
+  assert str(message).endswith("Cannot __sub__ Tiles with mismatched shapes (5, 7) and (7, 5)")
+
+def test___sub______08():
+  L = Tile((7, 5), np.ones((7, 5)))
+  R = Tile((5, 7))
+  with pt.raises(ValueError) as message:
+    T = L - R
+  assert str(message).endswith("Cannot __sub__ Tiles with mismatched shapes (7, 5) and (5, 7)")
+
+def test___sub______09():
+  L = Tile((5, 7),   np.ones((5, 7)))
+  R = Tile((5, 7), 2*np.ones((5, 7)))
+  T = L - R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == -1.0
+
+def test___sub______10():
+  L = Tile((5, 7), 2*np.ones((5, 7)))
+  R = Tile((5, 7),   np.ones((5, 7)))
+  T = L - R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 1.0
+
+def test___rmul_____01():
+  L = 2.0
+  R = Tile((5, 7))
+  T = L * R
+  assert isinstance(T, Tile)
+  assert T.is_empty()
+
+def test___rmul_____02():
+  L = 2.0
+  R = Tile((5, 7), np.ones((5, 7)))
+  T = L * R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 2.0
+
+def test___mul______01():
+  L = Tile((5, 7))
+  R = 2.0
+  T = L * R
+  assert isinstance(T, Tile)
+  assert T.is_empty()
+
+def test___mul______02():
+  L = Tile((5, 7), np.ones((5, 7)))
+  R = 2.0
+  T = L * R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 2.0
+
+def test___mul______03():
+  L = Tile((5, 7))
+  R = Tile((5, 7))
+  T = L * R
+  assert isinstance(T, Tile)
+  assert T.is_empty()
+
+def test___mul______04():
+  L = Tile((5, 7))
+  R = Tile((5, 7), np.ones((5, 7)))
+  T = L * R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 0.0
+
+def test___mul______05():
+  L = Tile((5, 7), np.ones((5, 7)))
+  R = Tile((5, 7))
+  T = L * R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 0.0
+
+def test___mul______06():
+  L = Tile((5, 7))
+  R = Tile((7, 5))
+  with pt.raises(ValueError) as message:
+    T = L * R
+  assert str(message).endswith("Cannot __mul__ Tiles with mismatched shapes (5, 7) and (7, 5)")
+
+def test___mul______07():
+  L = Tile((5, 7))
+  R = Tile((7, 5), np.ones((7, 5)))
+  with pt.raises(ValueError) as message:
+    T = L * R
+  assert str(message).endswith("Cannot __mul__ Tiles with mismatched shapes (5, 7) and (7, 5)")
+
+def test___mul______08():
+  L = Tile((7, 5), np.ones((7, 5)))
+  R = Tile((5, 7))
+  with pt.raises(ValueError) as message:
+    T = L * R
+  assert str(message).endswith("Cannot __mul__ Tiles with mismatched shapes (7, 5) and (5, 7)")
+
+def test___mul______09():
+  L = Tile((5, 7),   np.ones((5, 7)))
+  R = Tile((5, 7), 2*np.ones((5, 7)))
+  T = L * R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 2.0
+
+def test___mul______10():
+  L = Tile((5, 7), 2*np.ones((5, 7)))
+  R = Tile((5, 7),   np.ones((5, 7)))
+  T = L * R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 2.0
+
+def test___rdiv_____01():
+  L = 2.0
+  R = Tile((5, 7))
+  T = L / R
+  assert isinstance(T, Tile)
+  assert T.is_empty()
+
+def test___rdiv_____02():
+  L = 2.0
+  R = Tile((5, 7), np.ones((5, 7)))
+  T = L / R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 2.0
+
+def test___div______01():
+  L = Tile((5, 7))
+  R = 2.0
+  T = L / R
+  assert isinstance(T, Tile)
+  assert T.is_empty()
+
+def test___div______02():
+  L = Tile((5, 7), np.ones((5, 7)))
+  R = 2.0
+  T = L / R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 0.5
+
+def test___div______03():
+  L = Tile((5, 7))
+  R = Tile((5, 7))
+  T = L / R
+  assert isinstance(T, Tile)
+  assert T.is_empty()
+
+def test___div______04():
+  L = Tile((5, 7))
+  R = Tile((5, 7), np.ones((5, 7)))
+  T = L / R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 0.0
+
+def test___div______05():
+  L = Tile((5, 7), np.ones((5, 7)))
+  R = Tile((5, 7))
+  T = L / R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == np.inf
+
+def test___div______06():
+  L = Tile((5, 7))
+  R = Tile((7, 5))
+  with pt.raises(ValueError) as message:
+    T = L / R
+  assert str(message).endswith("Cannot __div__ Tiles with mismatched shapes (5, 7) and (7, 5)")
+
+def test___div______07():
+  L = Tile((5, 7))
+  R = Tile((7, 5), np.ones((7, 5)))
+  with pt.raises(ValueError) as message:
+    T = L / R
+  assert str(message).endswith("Cannot __div__ Tiles with mismatched shapes (5, 7) and (7, 5)")
+
+def test___div______08():
+  L = Tile((7, 5), np.ones((7, 5)))
+  R = Tile((5, 7))
+  with pt.raises(ValueError) as message:
+    T = L / R
+  assert str(message).endswith("Cannot __div__ Tiles with mismatched shapes (7, 5) and (5, 7)")
+
+def test___div______09():
+  L = Tile((5, 7),   np.ones((5, 7)))
+  R = Tile((5, 7), 2*np.ones((5, 7)))
+  T = L / R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 0.5
+
+def test___div______10():
+  L = Tile((5, 7), 2*np.ones((5, 7)))
+  R = Tile((5, 7),   np.ones((5, 7)))
+  T = L / R
+  assert isinstance(T, Tile)
+  for i in range(5):
+    for j in range(7):
+      assert T[i,j] == 2.0
+
--- symblock/tile.py
+++ symblock/tile.py
@@ -1,54 +1,41 @@
 import numpy as np
+import operator
 
 class Tile(object):
 
   def __init__(self, shape, array = None):
     if not array is None:
       if not isinstance(array, np.ndarray):
-        raise Exception("Cannot initialize Tile with object of type '{:s}'".format(type(array).__name__))
+        raise TypeError ("Cannot initialize Tile with object of type '{:s}'".format(type(array).__name__))
       elif not array.shape == shape:
-        raise Exception("Cannot initialize {:s}-shaped Tile with an array of shape {:s}".format(str(shape), str(array.shape)))
+        raise ValueError("Cannot initialize {:s}-shaped Tile with an array of shape {:s}".format(str(shape), str(array.shape)))
     self._shape   = shape
     self._array   = array
 
-  def get_shape  (self): return self._shape
-  def __nonzero__(self): return not self._array is None
-
-  def __repr__(self):
-    if self._array is None: return "Empty Tile"
-    return self._array.__repr__()
-
-  def __getitem__(self, *args):
-    if self._array is None: return 0.0
-    return self._array.__getitem__(*args)
-
+  def get_shape  (self       ): return self._shape
+  def is_empty   (self       ): return self._array is None
+  def __repr__   (self       ): return self._array.__str__()          if not self.is_empty() else "Empty Tile"
+  def __getitem__(self, *args): return self._array.__getitem__(*args) if not self.is_empty() else 0.0
   def __setitem__(self, *args):
-    if self._array is None: self._array = np.zeros(self._shape)
+    if self.is_empty(): self._array = np.zeros(self._shape)
     self._array.__setitem__(*args)
 
-  def __add__ (self, other): return self.binary_operation(other, np.ndarray.__add__ )
-  def __sub__ (self, other): return self.binary_operation(other, np.ndarray.__sub__ )
-  def __mul__ (self, other): return self.binary_operation(other, np.ndarray.__mul__ )
-  def __div__ (self, other): return self.binary_operation(other, np.ndarray.__div__ )
-  def __radd__(self, other): return self.binary_operation(other, np.ndarray.__radd__)
-  def __rsub__(self, other): return self.binary_operation(other, np.ndarray.__rsub__)
-  def __rmul__(self, other): return self.binary_operation(other, np.ndarray.__rmul__)
-  def __rdiv__(self, other): return self.binary_operation(other, np.ndarray.__rdiv__)
+  def __add__ (self, other): print('__add__ '); return self.binary_operation(other, operator.__add__)
+  def __sub__ (self, other): print('__sub__ '); return self.binary_operation(other, operator.__sub__)
+  def __mul__ (self, other): print('__mul__ '); return self.binary_operation(other, operator.__mul__)
+  def __div__ (self, other): print('__div__ '); return self.binary_operation(other, operator.__div__)
+  def __radd__(self, other): print('__radd__'); return self.binary_operation(other, operator.__add__, swap=True)
+  def __rsub__(self, other): print('__rsub__'); return self.binary_operation(other, operator.__sub__, swap=True)
+  def __rmul__(self, other): print('__rmul__'); return self.binary_operation(other, operator.__mul__, swap=True)
+  def __rdiv__(self, other): print('__rdiv__'); return self.binary_operation(other, operator.__div__, swap=True)
 
-  def binary_operation(self, other, operation):
-    if not self and (not other or not isinstance(other, Tile)):
+  def binary_operation(self, other, operation, swap=False):
+    if isinstance(other, Tile) and not self._shape == other._shape:
+      raise ValueError("Cannot {:s} Tiles with mismatched shapes {:s} and {:s}".format(operation.__name__, self._shape, other._shape))
+    if self.is_empty() and (not isinstance(other, Tile) or other.is_empty()):
       return Tile(self._shape, None)
-    larray = self._array if self else np.zeros(self._shape)
-    rarray = other
-    if isinstance(other, Tile):
-      if not self._shape == other._shape:
-        raise Exception("Cannot {:s} Tiles with mismatched shapes {:s} and {:s}".format(operation.__name__, self._shape, other._shape))
-      rarray = other._array if other else 0.0
-    return Tile(self._shape, operation(larray, rarray))
+    larray = self._array if not self.is_empty() else 0.0
+    rarray = other if not isinstance(other, Tile) else (other._array if not other.is_empty() else 0.0)
+    oarray = operation(larray, rarray) if not swap else operation(rarray, larray)
+    return Tile(self._shape, oarray)
 
-'''
-  def binary_operation(self, other, operation):
-    if bool(self) and bool(other):
-       operation(self._array, other._array)
-    else: return operation(self._array, 0.0) if bool(self) else 
-'''
