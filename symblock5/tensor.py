37a990a21447eeabe740182f168167495b2cf7a7
Andreas Copan <avcopan@gmail.com>
2015-11-23 17:10:48 -0500

    mas

--- symblock5/tensor.py
+++ symblock5/tensor.py
@@ -13,29 +13,29 @@ class tensor(object):
   def matmul(self, other):
     return np.tensordot(self, other, axes = (self.ndim - 1, 0))
 
-  def broadcast(self, shape, axes):
+  def broadcast(self, shape, axes=None):
     ndim = len(shape)
     if axes is None: axes = range(ndim - self.ndim, ndim)
     pmt = tuple(key for key in range(ndim) if not key in axes) + tuple(axes)
-    inv, _ = zip(*sorted(enumerate(pmt), key=lambda x:x[1]))
-    return np.broadcast_to(self, tuple(axes[key] for key in pmt)).transpose(inv)
+    inv, _ = zip(*sorted(enumerate(pmt), key=lambda x: x[1]))
+    return np.broadcast_to(self, tuple(shape[key] for key in pmt)).transpose(inv)
 
 
-class coretensor(np.ndarray, tensor):
+class coretensor(tensor, np.ndarray):
 
   def __new__(cls, shape):
-    return super(coretensor, cls).__new__(cls, shape)
+    return np.ndarray.__new__(cls, shape)
 
 
-class disktensor(np.memmap, tensor):
+class disktensor(tensor, np.memmap):
 
   def __new__(cls, shape, name = 'tmp'):
-    if name is 'tmp': filename = osp.join(mkdtemp(), 'disktensor.bin')
+    if name is 'tmp': filename = osp.join(mkdtemp(), 'tmp.bin')
     else:             filename = osp.join('/tmp', name + '.bin')
     if osp.isfile(filename):
-      return super(disktensor, cls).__new__(cls, filename, dtype=np.float64, mode='r+', shape=shape)
+      return np.memmap.__new__(cls, filename, dtype=np.float64, mode='r+', shape=shape)
     else:
-      return super(disktensor, cls).__new__(cls, filename, dtype=np.float64, mode='w+', shape=shape)
+      return np.memmap.__new__(cls, filename, dtype=np.float64, mode='w+', shape=shape)
 
 if __name__ == "__main__":
   d = disktensor((5, 5), name='myarray')
@@ -44,3 +44,15 @@ if __name__ == "__main__":
   e = disktensor((5, 5), name='myarray')
   e.fill(7)
   print d
+
+  a = disktensor((3, 7))
+  b = disktensor((7, 2))
+  a.fill(1)
+  b.fill(1)
+  c = a % b
+  print c
+
+  d = coretensor((5,))
+  for i in range(5): d[i] = i
+  print d.broadcast((5, 5))
+  print d.broadcast((5, 5), axes=(0,))
